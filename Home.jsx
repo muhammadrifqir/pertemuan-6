53d9bf3cb0de5e47495eaacc280fb0954fa2bdf5
muhammadrifqir <148336833+muhammadrifqir@users.noreply.github.com>
2024-04-30 23:25:36 +0700

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+/**
+ * Membuat component home
+ * menampilkan halaman home
+ * menampung navbar, main, dan footer component
+ */
+function Home () {
+    return (
+        <>
+        <h1>Halaman Home</h1>
+        </>
+    );
+}
+
+export default Home;
---
+++ Movies.jsx
@@ -0,0 +1,77 @@
+// Import Data Movies
+import Movies from "./Movies";
+import styles from "./movies.module.css";
+import data from "../utils/constants/data";
+import { useState } from "react";
+// import nanoid
+import { nanoid } from "nanoid";
+
+//component movie menerima props
+function Movie(props) {
+  // melakukan destructing props
+  const {movie} = props;
+
+  return (
+    <div className={styles.movie}>
+      <img className={styles.movie__image} src={movie.poster} alt={movie.title}></img>
+      <h3 className={styles.movie__title}>{movie.title}</h3>
+    </div>
+  );
+  }
+  
+  function handleClick() {
+    //menggunakan nanoid
+    const movie = {
+      id: nanoid(),
+      title: "jigsaw",
+      year: 2021,
+      type: "Movie",
+      poster: "https://picsum.photos/300/400",
+    };
+
+    // kode lain setelahnya
+  }
+
+  return (
+    <div className={styles.container}>
+      <section className={styles.movies}>
+        <h2 className={styles.movies__title}>Latest Movies</h2>
+        <div className={styles.movie__container}>
+          <div className={styles.movie}>
+            <img
+              className={styles.movie__image}
+              src="https://picsum.photos/300/400"
+              alt=""
+            ></img>
+            <h3 className={styles.movie__title}>Movie Title</h3>
+            <p className={styles.movie__date}>Date Title</p>
+          </div>
+          <div>
+            <div className={styles.container}>
+              <section className={styles.movies}>
+                <h2 className={styles.movies__title}>Latest Movies</h2>
+                <div className={styles.movie__container}>
+                  {/**
+                   * looping movies (array).
+                   * render component movie dan kirim props movie
+                   */}
+                  {movies.map((movie) => {
+                    return <Movie key={movie.id} movie={movie}></Movie>;
+                  })}
+                  <Movie title="Superman No Home" year="2022"></Movie>
+                  <Movie title="Dark" year="2021"></Movie>
+                  <Movie title="Turning Red" year="2022"></Movie>
+                  <Movie title="The Batman" year="2022"></Movie>
+                  <Movie title="Encanto" year="2021"></Movie>
+                </div>
+                {/** Element button diberikan event click: onClick */}
+                <button onClick={handleClick}>Add Movie</button>
+              </section>
+            </div>
+          </div>
+          {/* Duplicate 10 below */}
+        </div>
+      </section>
+    </div>
+  );
+export default Movies;
